9325478a1179f14e81a68345e68ecddcfabcd64b
thomas-haley <thomas.haley.2018@gmail.com>
2025-01-06 16:01:46 -0500

    Styled placeholder page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ComingSoonComponent } from './fallback/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'projects', component: HomeComponent},
+    {path: 'projects', component: ComingSoonComponent},
     {path: 'contact', component: HomeComponent},
 ];
--- src/app/fallback/coming-soon/coming-soon.component.ts
+++ src/app/fallback/coming-soon/coming-soon.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AnimatedBackgroundComponent } from "../../animated-background/animated-background.component";
 
 @Component({
   selector: 'app-coming-soon',
   standalone: true,
-  imports: [],
+  imports: [AnimatedBackgroundComponent],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css'
 })
